1231be6928d119bb4a129ea783f794bf95a57dc2
Jeff Seymour and Kyle Langley <altck8@gmail.com>
2016-01-11 10:28:08 -0800

    add integration spec for valid triangle

--- spec/triangle_integration_spec.rb
+++ spec/triangle_integration_spec.rb
@@ -7,7 +7,19 @@ set(:show_exceptions, false)
 
 describe('triangle result index path', {:type => :feature} ) do
   it('tests a valid triangle') do
-    # visit('/')
-
+    visit('/')
+    fill_in('a_field', :with => "10")
+    fill_in('b_field', :with => "15")
+    fill_in('c_field', :with => "20")
+    click_button('send')
+    expect(page).to have_content('Scalene Triangle')
+  end
+  it('tests a invalid triangle') do
+    visit('/')
+    fill_in('a_field', :with => "10")
+    fill_in('b_field', :with => "3")
+    fill_in('c_field', :with => "3")
+    click_button('send')
+    expect(page).to have_content('Not a valid triangle.')
   end
 end
